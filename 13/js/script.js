b4d9feae92c2e1bcb1baef622d97b5fc1c9f886e
BeniamG <beniamg@gmail.com>
2016-08-16 12:42:34 -0700

    Removed all draft pages from github to google drive

--- 13/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("navMenu").innerHTML =
-'<ul>'+
- '<li><a href="index.html">Home</a></li>'+
- '<li><a href="services.html">Services</a></li>'+
- '<li><a href="about.html">About</a></li>'+
-'</ul>';
